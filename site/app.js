f93d9ea2896baf8f550926d12c0d9ebe655a1eeb
Guy Royse <guy@guyroyse.com>
2018-01-29 20:21:50 -0500

    made it all pretty and stuff

--- site/app.js
+++ site/app.js
@@ -1,17 +1,22 @@
 var classinateUrl = 'https://ejitn71uqd.execute-api.us-east-1.amazonaws.com/prod/bigfootClassinator'
 
+var classMap = {
+    'class a': "You saw bigfoot! That's a Class A sighting.",
+    'class b': "You found some evidence of bigfoot like a footprint or something! That's a Class B sighting.",
+    'class c': "Someone told you about seeing bigfoot! That's a Class C sighting"
+}
+
 var app = new Vue({
     el: '#app',
     data: {
         report: '',
-        classification: ''
     },
     methods: {
         classinate: function() {
             this.$http.post(classinateUrl, { reportText: this.report })
                 .then(response => {
-                    console.log("There was a response", response);
-                    this.classification = response.body.reportClass;
+                    var reportClass = response.body.reportClass;
+                    window.alert(classMap[reportClass]);
                 }, error => {
                     console.log("There was an error", error);
                 });
