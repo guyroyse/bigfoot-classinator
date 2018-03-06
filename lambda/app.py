80a5194e20ec7d6e34039eca3e0dc3fc3c0a71a5
Guy Royse <guy@guyroyse.com>
2018-03-06 14:05:12 -0500

    refactored and added comments

--- lambda/app.py
+++ lambda/app.py
@@ -3,15 +3,28 @@ import os
 from nexosisapi import Client
 
 def classinate_bigfoot(event, context):
-    api_key = os.environ['NEXOSIS_API_KEY']
-    model_id = os.environ['BIGOOT_MODEL_ID']
-
+    # extract the report text from the passed in JSON
     report_text = event['reportText']
-    client = Client(api_key)
+
+    # create the Nexosis API client using the API key in an environment variable
+    nexosis_api_key = os.environ['NEXOSIS_API_KEY']
+    nexosis_client = Client(nexosis_api_key)
+
+    # the API can make several predictions at a time but we only want one so make this array
     features = [{ 'observed': report_text }]
+
+    # invoke the model to make a prediction using the model ID in an environment variable
+    model_id = os.environ['BIGFOOT_MODEL_ID']
     results = client.models.predict(model_id, features)
 
-    return {
-        'reportText': report_text,
-        'reportClass': results.data[0]['reportClass']
+    # since we only asked for one prediction, we only get one back
+    prediciton = results.data[0]
+
+    # map the prediction to something a little more front-end friendly
+    classination = {
+        'reportText': prediction['observed'],
+        'reportClass': prediction['reportClass']
     }
+
+    ## return all our hard work
+    return classination
