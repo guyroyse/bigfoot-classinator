4aaa05ee3fe7551f54c811b3e659493b9b106d9c
Guy Royse <guy@guyroyse.com>
2018-04-04 17:04:20 -0400

    updated styles, moved classination results out of an alert

--- site/app.js
+++ site/app.js
@@ -1,24 +1,30 @@
 var classinateUrl = 'https://rccptdhzsa.execute-api.us-east-1.amazonaws.com/prod/bigfootClassinator';
 
-var classMap = {
+var classMessages = {
     'class a': "You saw bigfoot! That's a Class A sighting.",
-    'class b': "You found some evidence of bigfoot like a footprint or something! That's a Class B sighting.",
+    'class b': "You found some evidence of bigfoot like a footprint! That's a Class B sighting.",
     'class c': "Someone told you about seeing bigfoot! That's a Class C sighting"
-}
+};
+
+var waitMessage = "Analyzing your sighting...";
+var errorMessage = "There was an error processing your classination.";
 
 var app = new Vue({
     el: '#app',
     data: {
         report: '',
+        classination: ''
     },
     methods: {
         classinate: function() {
+            this.classination = waitMessage;
             this.$http.post(classinateUrl, { reportText: this.report })
                 .then(response => {
                     var reportClass = response.body.reportClass;
-                    window.alert(classMap[reportClass]);
+                    this.classination = classMessages[reportClass];
                 }, error => {
-                    console.log("There was an error", error);
+                    console.log(error);
+                    this.classination = errorMessage;
                 });
         }
     }
